7d97635d5595ccf61ee5e2c137cf942e4630a2e7
SSSD <15829762162@163.com>
2023-12-25 23:35:06 +0800

    提取相关域名处理方法

--- src/main/java/top/sssd/ddns/common/utils/DoMainUtil.java
+++ src/main/java/top/sssd/ddns/common/utils/DoMainUtil.java
@@ -24,4 +24,32 @@ public class DoMainUtil {
         return count == 1;
     }
 
+    public static String[] spiltDomain(String domain){
+        String resultDomain = "";
+        String subDoMain = "";
+        if (DoMainUtil.firstLevel(domain)) {
+            subDoMain = "@";
+        } else {
+            resultDomain = domain.substring(domain.indexOf('.') + 1);
+            subDoMain = domain.substring(0, domain.indexOf('.'));
+        }
+        return new String[]{resultDomain,subDoMain};
+    }
+
+    public static  int findNthOccurrence(String str, String subStr, int n) {
+        // 记录出现次数
+        int count = 0;
+        // 从后往前查找最后一次出现的位置
+        int index = str.lastIndexOf(subStr);
+        // 如果找到了并且出现次数小于n
+        while (index != -1 && count < n) {
+            // 继续往前查找下一次出现的位置
+            index = str.lastIndexOf(subStr, index - 1);
+            // 更新出现次数
+            count++;
+        }
+        // 返回最后一次出现的位置的索引
+        return index;
+    }
+
 }
